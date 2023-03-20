05429db00b2af0c3bea8c1504919c0a5a4856bdb
Alan Albertengo <Alan.Albertengo@gmail.com>
2023-03-20 10:46:09 -0300

    add post view

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
+<script lang="ts" setup>
+import NavBar from "./components/NavBar.vue";
+</script>
+
 <template>
   <div class="section">
     <div class="container">
+      <NavBar />
       <RouterView />
     </div>
   </div>
