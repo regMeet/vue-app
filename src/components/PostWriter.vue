17d43e5520ab0525bc47ccdd29232bf8133e31d6
Alan Albertengo <Alan.Albertengo@gmail.com>
2023-03-20 12:58:01 -0300

    added markdown and watch properties

---
+++ src/components/PostWriter.vue
@@ -0,0 +1,66 @@
+<script lang="ts" setup>
+import { ref, onMounted, watch, watchEffect } from "vue";
+import { TimelinePost } from "../posts";
+import { marked } from "marked";
+
+const props = defineProps<{
+  post: TimelinePost;
+}>();
+
+const title = ref(props.post.title);
+const content = ref(props.post.markdown);
+const html = ref("");
+const contentEditable = ref<HTMLDivElement>();
+
+watchEffect(() => {
+  marked.parse(content.value, (err, parseResult) => {
+    html.value = parseResult;
+  });
+});
+
+// watch(
+//   content,
+//   (newContent) => {
+//     marked.parse(newContent, (err, parseResult) => {
+//       html.value = parseResult;
+//     });
+//   },
+//   {
+//     immediate: true,
+//   }
+// );
+
+onMounted(() => {
+  if (!contentEditable.value) {
+    throw Error("ContentEditable DOM node was not found.");
+  }
+  contentEditable.value.innerText = content.value;
+});
+
+function handleInput() {
+  if (!contentEditable.value) {
+    throw Error("ContentEditable DOM node was not found.");
+  }
+  content.value = contentEditable.value.innerText;
+}
+</script>
+
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="field">
+        <div class="label">Post title</div>
+        <input type="text" class="input" v-model="title" />
+      </div>
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column">
+      <div contenteditable ref="contentEditable" @input="handleInput" />
+    </div>
+    <div class="column">
+      <div v-html="html" />
+    </div>
+  </div>
+</template>
--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,1 +1,14 @@
-<template>New Post</template>
+<script setup lang="ts">
+import { DateTime } from "luxon";
+import PostWriter from "../components/PostWriter.vue";
+import { TimelinePost } from "../posts";
+
+const post: TimelinePost = {
+  id: "-1",
+  title: "Title",
+  created: DateTime.now(),
+  markdown: "## Title",
+};
+</script>
+
+<template>New Post <PostWriter :post="post" /></template>
