e19c0da1ac3c087ab92572e1ce30311b81fdf799
Alan Albertengo <Alan.Albertengo@gmail.com>
2023-03-20 14:27:50 -0300

    add empty modal

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <script lang="ts" setup>
+import { computed } from "vue";
 import NavBar from "./components/NavBar.vue";
+import { useModal } from "./composables/modal";
+
+const modal = useModal();
+
+const modalStyle = computed(() => {
+  return {
+    display: modal.show.value ? "block" : "none",
+  };
+});
 </script>
 
 <template>
+  <div class="modal" style="color: white" :style="modalStyle">
+    <div class="modal-background">
+      <div class="modal-content">
+        <div id="modal">content</div>
+      </div>
+    </div>
+    <button class="modal-close is-large" @click="modal.hideModal()"></button>
+  </div>
   <div class="section">
     <div class="container">
       <NavBar />
