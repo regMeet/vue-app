8d7a7c0a440bbbe31623481c9fac197f43d8e269
Alan Albertengo <Alan.Albertengo@santexgroup.com>
2023-04-03 02:58:53 -0300

    use same port for front and api

--- src/stores/users.ts
+++ src/stores/users.ts
@@ -6,7 +6,7 @@ export const useUsers = defineStore("users", {
     createUser(newUser: NewUser) {
       const body = JSON.stringify(newUser);
 
-      return window.fetch("http://localhost:8000/users", {
+      return window.fetch("/api/users", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
